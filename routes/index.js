a9e2a167e3e47f92679d1f63ecbbf1c2a80588e6
Fadyy22 <fadyalaa441@gmail.com>
2024-03-25 21:48:20 +0200

    mount company routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const authRoute = require('./authRoute');
+const userRoute = require('./userRoute');
+const companyRoute = require('./companyRoute');
 
 const mountRoutes = (app) => {
   app.use('/auth', authRoute);
   app.use('/users', userRoute);
+  app.use('/company', companyRoute);
 };
 
 module.exports = mountRoutes;
