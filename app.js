4930f166c45d16b88bacd842c823116918a79d37
Fadyy22 <fadyalaa441@gmail.com>
2024-03-25 23:47:02 +0200

    add 404 route and global error handler

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+const ApiError = require('./utils/apiError');
 const dbConnection = require('./config/database');
 const mountRoutes = require('./routes');
 
@@ -11,5 +12,17 @@ const app = express();
 app.use(express.json({ limit: '20kb' }));
 
 mountRoutes(app);
+
+app.all('*', (req, res, next) => {
+  next(new ApiError(`Can't find this route ${req.originalUrl}`, 404));
+});
+
+app.use((error, req, res, next) => {
+  error.statusCode = error.statusCode || 500;
+  res.status(error.statusCode).json({
+    error: error,
+    message: error.message,
+  });
+});
 
 dbConnection(app);
